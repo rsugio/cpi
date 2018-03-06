8e81c9fdde3418fc24b11f3221636a652c120a47
chumpa <chumpa@yandex.ru>
2018-03-06 08:51:30 +0300

    exception handling
    Signed-off-by: chumpa <chumpa@yandex.ru>

--- rsugio-cpi-008-exception/src/main/resources/script/rsugio-cpi-008-exception.groovy
+++ rsugio-cpi-008-exception/src/main/resources/script/rsugio-cpi-008-exception.groovy
@@ -3,11 +3,14 @@ import org.apache.camel.component.ahc.AhcOperationFailedException
 
 CpiMsg init(CpiMsg msg) {
 	String body = msg.getBody(String)
-	msg.properties.url = body ?: "http://httpbin.org/azaza"
+	
+	msg.properties.url = body ? "http://s.rsug.io/$body" as String : "http://s.rsug.io/503"
+	msg.setBody("Empty payload")
 	msg
 }
 
 CpiMsg checkException(CpiMsg msg) {
+	def mpl = messageLogFactory.getMessageLog(msg)
 	Exception e = msg.properties.CamelExceptionCaught
 	if (e instanceof AhcOperationFailedException) {
 		AhcOperationFailedException ahc = (AhcOperationFailedException)e
@@ -15,13 +18,42 @@ CpiMsg checkException(CpiMsg msg) {
 		String rl = ahc.redirectLocation
 		String rt = ahc.statusText
 		int rc = ahc.statusCode
-		if (rc==404) {
-			msg.properties.isescalated = "true"
-		} else if (rc==302) {
-			msg.properties.iserror = "true"
+		if (rc==500) {
+			msg.properties.escalate = "true"
+			msg.setBody("Process escalated for error code $rc" as String)
+		} else if (rc==501) {
+			msg.properties.error = "true"
+			msg.setBody("Process failed for error code $rc" as String)
+		} else {
+		    msg.setBody("Error ignored for code $rc" as String)   
 		}
-//		msg.properties.remove("CamelExceptionCaught")
+		mpl.addAttachmentAsString("CamelExceptionCaught", e.toString(), "text/plain")
+		msg.properties.remove("CamelExceptionCaught")
+	} else {
+		mpl.addAttachmentAsString("CamelExceptionCaught", e.toString(), "text/plain")
 	}
+//	mpl.addAttachmentAsString("properties", msg.properties.toString(), "text/plain")
+//	mpl.addAttachmentAsString("headers", msg.headers.toString(), "text/plain")
+//	msg.properties.remove("CamelFailureRouteId")
+//	msg.properties.remove("CamelFailureEndpoint")
+//	msg.properties.remove("SAP_ErrorModelStepID")
+	msg.headers.CamelHttpResponseCode = 200
+	msg.headers.CamelHttpResponseText = "OK"
+	
+	msg
+}
 
+CpiMsg log(CpiMsg msg) {
+	def mpl = messageLogFactory.getMessageLog(msg)
+	String s = """error: ${msg.properties.error}
+escalated: ${msg.properties.escalated}
+"""
+	mpl.addAttachmentAsString("log", s, "text/plain")
+	msg
+}
+
+CpiMsg whenNoError(CpiMsg msg) {
+	def mpl = messageLogFactory.getMessageLog(msg)
+	msg.setBody("No exception")
 	msg
 }
